2aadecf683345706cba08c2a9b4ad57909111f27
Dev-Rizz <108243293+Dev-Rizz@users.noreply.github.com>
2023-05-30 00:51:40 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -67,7 +67,7 @@ VALIDITY() {
 IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/auth/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
     echo -e "\e[32mPermission Accepted...\e[0m"
-    VALIDITY
+    
 else
     echo -e "\e[31mPermission Denied!\e[0m"
     echo -e "\e[31mPlease buy script first\e[0m"
